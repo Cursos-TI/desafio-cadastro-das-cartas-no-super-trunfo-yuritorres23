19df404933e5f5d9235f0ff5e0adccf7144010f6
yuritorres23 <23yurigabriel@gmail.com>
2025-03-19 18:47:17 +0000

    nomes cidades

--- super trunfo novato e aventureiro.c
+++ super trunfo novato e aventureiro.c
@@ -3,6 +3,7 @@
 int main(){
     // variaveis primeira carta
     char carta1[3];
+    char nome_cidade1[20];
     int populacao1;
     float area1, pib1;
     int pontos_turisticos1;
@@ -11,6 +12,7 @@ int main(){
 
     // variaveis segunda carta
     char carta2[3];
+    char nome_cidade2[20];
     int populacao2;
     float area2, pib2;
     int pontos_turisticos2;
@@ -20,6 +22,8 @@ int main(){
     // cadastro primeira carta
     printf("Digite o codigo da primeira carta(ex: A01)");
     scanf("%s", carta1);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nome_cidade1);
     printf("Digite a população:");
     scanf("%d", &populacao1);
     printf("Digite a área (em km²): ");
@@ -38,6 +42,8 @@ int main(){
     //cadastro segunda carta
     printf("\nDigite o codigo da segunda carta(ex: B02): ");
     scanf("%s", carta2);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", nome_cidade2);
     printf("Digite a população: ");
     scanf("%d", &populacao2);
     printf("Digite a área (em km²): ");
@@ -55,10 +61,10 @@ int main(){
 
     //exibição dados cadastrados
     printf("\n--- Dados das Cartas ---\n");
-    printf("\nCarta: %s\nPopulação: %d\nÁrea: %.2f km²\nPontos Turisticos: %d\nDensidade populacional: %.2f\nPIB per capta: %.2f\n",
-         carta1, populacao1, area1, pib1, pontos_turisticos1, densidade_populacional1, PIB_per_capta1);
-    printf("\nCarta: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos Turisticos: %d\nDensidade populacional: %.2f\nPIB per capta: %.2f\n",
-         carta2, populacao2, area2, pib2, pontos_turisticos2, densidade_populacional2, PIB_per_capta2 );
+    printf("\nCarta: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPontos Turisticos: %d\nDensidade populacional: %.2f\nPIB per capta: %.2f\n",
+         carta1, nome_cidade1, populacao1, area1, pib1, pontos_turisticos1, densidade_populacional1, PIB_per_capta1);
+    printf("\nCarta: %s\nNome da Cidade: \nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos Turisticos: %d\nDensidade populacional: %.2f\nPIB per capta: %.2f\n",
+         carta2, nome_cidade2, populacao2, area2, pib2, pontos_turisticos2, densidade_populacional2, PIB_per_capta2 );
 
     return 0;
 
